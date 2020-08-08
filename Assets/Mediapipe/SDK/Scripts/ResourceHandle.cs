14a1dadbbdb31baea3398a098bcb603156c81d05
homuler <eulerdora.1810.mann@gmail.com>
2020-08-08 15:19:46 +0900

    refactor: implement ResourceHandle class

---
+++ Assets/Mediapipe/SDK/Scripts/ResourceHandle.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Mediapipe {
+  public abstract class ResourceHandle : IDisposable {
+    protected bool isOwner;
+    protected IntPtr ptr;
+
+    protected ResourceHandle() {}
+
+    protected ResourceHandle(IntPtr ptr, bool isOwner = true) {
+      this.ptr = ptr;
+      this.isOwner = isOwner;
+    }
+
+    ~ResourceHandle() => Dispose(false);
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected abstract void Dispose(bool disposing);
+
+    public IntPtr GetPtr() {
+      return ptr;
+    }
+
+    public virtual void Release() {
+      isOwner = false;
+      ptr = IntPtr.Zero;
+    }
+
+    protected bool OwnsResource() {
+      return isOwner && ptr != IntPtr.Zero;
+    }
+  }
+}
