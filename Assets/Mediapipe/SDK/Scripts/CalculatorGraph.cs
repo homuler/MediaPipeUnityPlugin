f6ea9c8143896cd967c91854cc00e9b87df8cb71
homuler <eulerdora.1810.mann@gmail.com>
2020-08-08 18:35:09 +0900

    refactor: CalculatorGraph inherits ResourceHandle

--- Assets/Mediapipe/SDK/Scripts/CalculatorGraph.cs
+++ Assets/Mediapipe/SDK/Scripts/CalculatorGraph.cs
@@ -1,16 +1,15 @@
-using MpCalculatorGraph = System.IntPtr;
+using System;
+
 using MpPacket = System.IntPtr;
-using MpStatus = System.IntPtr;
 using MpStatusOrPoller = System.IntPtr;
 
 namespace Mediapipe {
-  public class CalculatorGraph {
+  public class CalculatorGraph : ResourceHandle {
+    private bool _disposed = false;
     private CalculatorGraphConfig graphConfig;
-    public MpCalculatorGraph mpCalculatorGraph;
 
-    public CalculatorGraph(string configText) {
+    public CalculatorGraph(string configText) : base(UnsafeNativeMethods.MpCalculatorGraphCreate()) {
       graphConfig = new CalculatorGraphConfig(configText);
-      mpCalculatorGraph = UnsafeNativeMethods.MpCalculatorGraphCreate();
 
       var status = Initialize(graphConfig);
 
@@ -19,32 +18,41 @@ namespace Mediapipe {
       }
     }
 
-    ~CalculatorGraph() {
-      UnsafeNativeMethods.MpCalculatorGraphDestroy(mpCalculatorGraph);
+    protected override void Dispose(bool disposing) {
+      if (_disposed) return;
+
+      if (OwnsResource()) {
+        UnsafeNativeMethods.MpCalculatorGraphDestroy(ptr);
+      }
+
+      ptr = IntPtr.Zero;
+      graphConfig = null;
+
+      _disposed = true;
     }
 
     public Status StartRun(SidePacket sidePacket) {
-      return new Status(UnsafeNativeMethods.MpCalculatorGraphStartRun(mpCalculatorGraph, sidePacket.GetPtr()));
+      return new Status(UnsafeNativeMethods.MpCalculatorGraphStartRun(ptr, sidePacket.GetPtr()));
     }
 
     public Status WaitUntilDone() {
-      return new Status(UnsafeNativeMethods.MpCalculatorGraphWaitUntilDone(mpCalculatorGraph));
+      return new Status(UnsafeNativeMethods.MpCalculatorGraphWaitUntilDone(ptr));
     }
 
     public Status CloseInputStream(string name) {
-      return new Status(UnsafeNativeMethods.MpCalculatorGraphCloseInputStream(mpCalculatorGraph, name));
+      return new Status(UnsafeNativeMethods.MpCalculatorGraphCloseInputStream(ptr, name));
     }
 
     private Status Initialize(CalculatorGraphConfig config) {
-      return new Status(UnsafeNativeMethods.MpCalculatorGraphInitialize(mpCalculatorGraph, config.GetPtr()));
+      return new Status(UnsafeNativeMethods.MpCalculatorGraphInitialize(ptr, config.GetPtr()));
     }
 
     protected MpStatusOrPoller AddOutputStreamPoller(string name) {
-      return UnsafeNativeMethods.MpCalculatorGraphAddOutputStreamPoller(mpCalculatorGraph, name);
+      return UnsafeNativeMethods.MpCalculatorGraphAddOutputStreamPoller(ptr, name);
     }
 
     protected Status AddPacketToInputStream(string name, MpPacket packetPtr) {
-      return new Status(UnsafeNativeMethods.MpCalculatorGraphAddPacketToInputStream(mpCalculatorGraph, name, packetPtr));
+      return new Status(UnsafeNativeMethods.MpCalculatorGraphAddPacketToInputStream(ptr, name, packetPtr));
     }
   }
 }
