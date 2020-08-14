7eb8928aad69d656c6978d7b934ceaad952ab60f
homuler <eulerdora.1810.mann@gmail.com>
2020-08-14 09:44:41 +0900

    wip: implement DirectorOnGPU

--- Assets/Mediapipe/Examples/Scripts/Director.cs
+++ Assets/Mediapipe/Examples/Scripts/Director.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 using Mediapipe;
 using UnityEngine;
 
--- Assets/Mediapipe/Examples/Scripts/DirectorOnGPU.cs
+++ Assets/Mediapipe/Examples/Scripts/DirectorOnGPU.cs
@@ -2,13 +2,74 @@ using Mediapipe;
 using UnityEngine;
 
 public class DirectorOnGPU : Director {
-  private DemoGraph calculatorGraph;
+  private DemoGraphOnGPU calculatorGraph;
+  private GlCalculatorHelper glCalculatorHelper;
 
   [SerializeField] TextAsset config;
 
   protected override void Start() {
     base.Start();
+
+    calculatorGraph = new DemoGraphOnGPU(config.text);
+    SetupGpu();
+    calculatorGraph.InitOutputStreamPoller();
+    calculatorGraph.StartRun().AssertOk();
+
+    webCamScreen.GetComponent<WebCamScreenController>().OnFrameRender.AddListener(RunGraph);
   }
 
-  void RunGraph(Color32[] pixelData, int width, int height) {}
+  void SetupGpu() {
+    var gpuResources = new StatusOrGpuResources().ConsumeValue();
+
+    calculatorGraph.SetGpuResources(gpuResources).AssertOk();
+
+    glCalculatorHelper = new GlCalculatorHelper();
+    glCalculatorHelper.InitializeForTest(calculatorGraph.GetGpuResources());
+  }
+
+  void RunGraph(Color32[] pixelData, int width, int height) {
+    int timestamp = System.Environment.TickCount & System.Int32.MaxValue;
+    var imageFrame = ImageFrame.FromPixels32(pixelData, width, height);
+
+    glCalculatorHelper.RunInGlContext(() => {
+      var texture = glCalculatorHelper.CreateSourceTexture(imageFrame);
+      var gpuFrame = texture.GetGpuBufferFrame();
+
+      UnsafeNativeMethods.GlFlush();
+      texture.Release();
+
+      calculatorGraph.AddPacketToInputStream(new GpuBufferPacket(gpuFrame, timestamp)).AssertOk();
+
+      return Status.OkStatus();
+    }).AssertOk();
+
+/**
+    var outputStreamPoller = calculatorGraph.outputStreamPoller;
+    var packet = new GpuBufferPacket();
+    ImageFrame outputFrame = null;
+
+    if (!outputStreamPoller.Next(packet)) return;
+
+    glCalculatorHelper.RunInGlContext(() => {
+      var gpuFrame = packet.ConsumeValue();
+      var gpuFrameFormat = gpuFrame.Format();
+      var texture = glCalculatorHelper.CreateSourceTexture(gpuFrame);
+
+      outputFrame = new ImageFrame(
+        gpuFrameFormat.ImageFormatFor(), gpuFrame.Width(), gpuFrame.Height(), ImageFrame.kGlDefaultAlignmentBoundary);
+
+      glCalculatorHelper.BindFramebuffer(texture);
+      var info = gpuFrameFormat.GlTextureInfoFor(0);
+
+      UnsafeNativeMethods.GlReadPixels(0, 0, texture.Width(), texture.Height(), info.GlFormat(), info.GlType(), outputFrame.PixelDataPtr());
+      UnsafeNativeMethods.GlFlush();
+
+      texture.Release();
+      return Status.OkStatus();
+    }).AssertOk();
+
+    Color32[] colors = outputFrame.GetColor32s();
+*/
+    webCamScreen.GetComponent<WebCamScreenController>().DrawScreen(pixelData);
+  }
 }
