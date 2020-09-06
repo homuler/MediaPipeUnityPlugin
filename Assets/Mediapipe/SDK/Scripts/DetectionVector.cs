7bff20a4173aba6bdd5a567250a6294b59561bd6
homuler <eulerdora.1810.mann@gmail.com>
2020-09-07 00:30:41 +0900

    refactor!: use protobuf to receive `Detection` data

--- Assets/Mediapipe/SDK/Scripts/DetectionVector.cs
+++ Assets/Mediapipe/SDK/Scripts/DetectionVector.cs
@@ -1,19 +1,34 @@
 using System;
 using System.Runtime.InteropServices;
 
-using MpDetection = System.IntPtr;
 using MpDetectionVector = System.IntPtr;
 
 namespace Mediapipe {
   [StructLayout(LayoutKind.Sequential)]
   public unsafe struct DetectionVector {
-    public MpDetection* detections;
+    public byte** detections;
+    public int* sizeList;
     public int size;
 
     public static Detection[] PtrToDetectionArray(MpDetectionVector detectionVectorPtr) {
       var detectionVector = Marshal.PtrToStructure<DetectionVector>(detectionVectorPtr);
+      var detections = new Detection[detectionVector.size];
 
-      return Detection.PtrToArray((IntPtr)detectionVector.detections, detectionVector.size);
+      unsafe {
+        var arr = detectionVector.detections;
+        var sizePtr = detectionVector.sizeList;
+
+        for (var i = 0; i < detectionVector.size; i++) {
+          var size = *sizePtr++;
+          var bytes = new byte[size];
+
+          Marshal.Copy((IntPtr)(*arr++), bytes, 0, size);
+
+          detections[i] = Detection.Parser.ParseFrom(bytes);
+        }
+      }
+
+      return detections;
     }
   }
 }
