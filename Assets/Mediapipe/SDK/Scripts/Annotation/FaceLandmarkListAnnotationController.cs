f7771bcd4b50e36ef49b5d6bde972db68b6e6ced
homuler <eulerdora.1810.mann@gmail.com>
2020-09-21 09:55:42 +0900

    refactor: implement LandmarkListAnnotationController

--- Assets/Mediapipe/SDK/Scripts/Annotation/FaceLandmarkListAnnotationController.cs
+++ Assets/Mediapipe/SDK/Scripts/Annotation/FaceLandmarkListAnnotationController.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 using Edge = System.Tuple<int ,int>;
 
 namespace Mediapipe {
-  public class FaceLandmarkListAnnotationController : MonoBehaviour {
-    private static readonly IList<Edge> connections = new List<Edge> {
+  public class FaceLandmarkListAnnotationController : LandmarkListAnnotationController {
+    private static readonly IList<Edge> _Connections = new List<Edge> {
       // Face Oval
       new Edge(10, 338),
       new Edge(338, 297),
@@ -139,65 +138,12 @@ namespace Mediapipe {
       new Edge(409, 291),
     };
 
-    private static readonly int NodeSize = 468;
-    private static readonly int EdgeSize = connections.Count;
-
-    [SerializeField] GameObject nodePrefab = null;
-    [SerializeField] GameObject edgePrefab = null;
-
-    private List<GameObject> nodes = new List<GameObject>(NodeSize);
-    private List<GameObject> edges = new List<GameObject>(EdgeSize);
-
-    void Awake() {
-      for (var i = 0; i < NodeSize; i++) {
-        nodes.Add(Instantiate(nodePrefab));
-      }
-
-      for (var i = 0; i < EdgeSize; i++) {
-        edges.Add(Instantiate(edgePrefab));
-      }
+    protected override IList<Edge> Connections {
+      get { return _Connections; }
     }
 
-    public void Clear() {
-      foreach (var landmark in nodes) {
-        landmark.GetComponent<NodeAnnotationController>().Clear();
-      }
-
-      foreach (var line in edges) {
-        line.GetComponent<EdgeAnnotationController>().Clear();
-      }
-    }
-
-    /// <summary>
-    ///   Renders face landmarks on a screen.
-    ///   It is assumed that the screen vertical to terrain and not inverted.
-    /// </summary>
-    /// <param name="isFlipped">
-    ///   if true, x axis is oriented from right to left (top-right point is (0, 0) and bottom-left is (1, 1))
-    /// </param>
-    /// <remarks>
-    ///   In <paramref name="landmarkList" />, y-axis is oriented from top to bottom.
-    /// </remarks>
-    public void Draw(Transform screenTransform, NormalizedLandmarkList landmarkList, bool isFlipped = false) {
-      var localScale = screenTransform.localScale;
-      var scale = new Vector3(10 * localScale.x, 10 * localScale.z, 1);
-
-      for (var i = 0; i < NodeSize; i++) {
-        var landmark = landmarkList.Landmark[i];
-        var node = nodes[i];
-
-        node.GetComponent<NodeAnnotationController>().Draw(screenTransform, landmark, isFlipped, 0.3f);
-      }
-
-      for (var i = 0; i < EdgeSize; i++) {
-        var connection = connections[i];
-        var edge = edges[i];
-
-        var a = nodes[connection.Item1];
-        var b = nodes[connection.Item2];
-
-        edge.GetComponent<EdgeAnnotationController>().Draw(screenTransform, a, b);
-      }
+    protected override int NodeSize {
+      get { return 468; }
     }
   }
 }
--- Assets/Mediapipe/SDK/Scripts/Annotation/HandLandmarkListAnnotationController.cs
+++ Assets/Mediapipe/SDK/Scripts/Annotation/HandLandmarkListAnnotationController.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 
 using Edge = System.Tuple<int ,int>;
 
 namespace Mediapipe {
-  public class HandLandmarkListAnnotationController : MonoBehaviour {
-    private static readonly IList<Edge> connections = new List<Edge> {
+  public class HandLandmarkListAnnotationController : LandmarkListAnnotationController {
+    protected static readonly IList<Edge> _Connections = new List<Edge> {
       new Edge(0, 1),
       new Edge(1, 2),
       new Edge(2, 3),
@@ -30,76 +28,12 @@ namespace Mediapipe {
       new Edge(19, 20),
     };
 
-    private static readonly int NodeSize = 21;
-    private static readonly int EdgeSize = connections.Count;
-
-    [SerializeField] GameObject nodePrefab = null;
-    [SerializeField] GameObject edgePrefab = null;
-
-    private List<GameObject> nodes = new List<GameObject>(NodeSize);
-    private List<GameObject> edges = new List<GameObject>(EdgeSize);
-
-    void Awake() {
-      for (var i = 0; i < NodeSize; i++) {
-        nodes.Add(Instantiate(nodePrefab));
-      }
-
-      for (var i = 0; i < EdgeSize; i++) {
-        edges.Add(Instantiate(edgePrefab));
-      }
+    protected override IList<Edge> Connections {
+      get { return _Connections; }
     }
 
-    public void Clear() {
-      foreach (var landmark in nodes) {
-        landmark.GetComponent<NodeAnnotationController>().Clear();
-      }
-
-      foreach (var line in edges) {
-        line.GetComponent<EdgeAnnotationController>().Clear();
-      }
-    }
-
-    /// <summary>
-    ///   Renders hand landmarks on a screen.
-    ///   It is assumed that the screen vertical to terrain and not inverted.
-    /// </summary>
-    /// <param name="isFlipped">
-    ///   if true, x axis is oriented from right to left (top-right point is (0, 0) and bottom-left is (1, 1))
-    /// </param>
-    /// <remarks>
-    ///   In <paramref name="landmarkList" />, y-axis is oriented from top to bottom.
-    /// </remarks>
-    public void Draw(Transform screenTransform, NormalizedLandmarkList landmarkList, bool isFlipped = false) {
-      if (isEmpty(landmarkList)) {
-        Clear();
-        return;
-      }
-
-      var localScale = screenTransform.localScale;
-      var scale = new Vector3(10 * localScale.x, 10 * localScale.z, 1);
-
-      for (var i = 0; i < NodeSize; i++) {
-        var landmark = landmarkList.Landmark[i];
-        var node = nodes[i];
-
-        node.GetComponent<NodeAnnotationController>().Draw(screenTransform, landmark, isFlipped);
-      }
-
-      for (var i = 0; i < EdgeSize; i++) {
-        var connection = connections[i];
-        var edge = edges[i];
-
-        var a = nodes[connection.Item1];
-        var b = nodes[connection.Item2];
-
-        edge.GetComponent<EdgeAnnotationController>().Draw(screenTransform, a, b);
-      }
-    }
-
-    private bool isEmpty(NormalizedLandmarkList landmarkList) {
-      return landmarkList.Landmark.All(landmark => {
-        return landmark.X == 0 && landmark.Y == 0 && landmark.Z == 0;
-      });
+    protected override int NodeSize {
+      get { return 21; }
     }
   }
 }
