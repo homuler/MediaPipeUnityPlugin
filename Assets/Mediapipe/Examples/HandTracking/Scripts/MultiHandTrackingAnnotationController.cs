8f889e75c2d71d8d5cafa79e7e0b22871b6ff9ae
homuler <eulerdora.1810.mann@gmail.com>
2020-09-22 11:02:14 +0900

    refactor: change namespace

--- Assets/Mediapipe/Examples/HandTracking/Scripts/MultiHandTrackingAnnotationController.cs
+++ Assets/Mediapipe/Examples/HandTracking/Scripts/MultiHandTrackingAnnotationController.cs
@@ -1,34 +1,33 @@
+using Mediapipe;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mediapipe {
-  public class MultiHandTrackingAnnotationController : AnnotationController {
-    [SerializeField] GameObject handLandmarkListsPrefab = null;
-    [SerializeField] GameObject palmDetectionsPrefab = null;
-    [SerializeField] GameObject palmRectsPrefab = null;
+public class MultiHandTrackingAnnotationController : AnnotationController {
+  [SerializeField] GameObject handLandmarkListsPrefab = null;
+  [SerializeField] GameObject palmDetectionsPrefab = null;
+  [SerializeField] GameObject palmRectsPrefab = null;
 
-    private GameObject handLandmarkListsAnnotation;
-    private GameObject palmDetectionsAnnotation;
-    private GameObject palmRectsAnnotation;
+  private GameObject handLandmarkListsAnnotation;
+  private GameObject palmDetectionsAnnotation;
+  private GameObject palmRectsAnnotation;
 
-    void Awake() {
-      handLandmarkListsAnnotation = Instantiate(handLandmarkListsPrefab);
-      palmDetectionsAnnotation = Instantiate(palmDetectionsPrefab);
-      palmRectsAnnotation = Instantiate(palmRectsPrefab);
-    }
+  void Awake() {
+    handLandmarkListsAnnotation = Instantiate(handLandmarkListsPrefab);
+    palmDetectionsAnnotation = Instantiate(palmDetectionsPrefab);
+    palmRectsAnnotation = Instantiate(palmRectsPrefab);
+  }
 
-    public override void Clear() {
-      handLandmarkListsAnnotation.GetComponent<MultiHandLandmarkListAnnotationController>().Clear();
-      palmDetectionsAnnotation.GetComponent<DetectionListAnnotationController>().Clear();
-      palmRectsAnnotation.GetComponent<RectListAnnotationController>().Clear();
-    }
+  public override void Clear() {
+    handLandmarkListsAnnotation.GetComponent<MultiHandLandmarkListAnnotationController>().Clear();
+    palmDetectionsAnnotation.GetComponent<DetectionListAnnotationController>().Clear();
+    palmRectsAnnotation.GetComponent<RectListAnnotationController>().Clear();
+  }
 
-    public void Draw(Transform screenTransform, List<NormalizedLandmarkList> handLandmarkLists,
-        List<Detection> palmDetections, List<NormalizedRect> palmRects, bool isFlipped = false)
-    {
-      handLandmarkListsAnnotation.GetComponent<MultiHandLandmarkListAnnotationController>().Draw(screenTransform, handLandmarkLists, isFlipped);
-      palmDetectionsAnnotation.GetComponent<DetectionListAnnotationController>().Draw(screenTransform, palmDetections, isFlipped);
-      palmRectsAnnotation.GetComponent<RectListAnnotationController>().Draw(screenTransform, palmRects, isFlipped);
-    }
+  public void Draw(Transform screenTransform, List<NormalizedLandmarkList> handLandmarkLists,
+      List<Detection> palmDetections, List<NormalizedRect> palmRects, bool isFlipped = false)
+  {
+    handLandmarkListsAnnotation.GetComponent<MultiHandLandmarkListAnnotationController>().Draw(screenTransform, handLandmarkLists, isFlipped);
+    palmDetectionsAnnotation.GetComponent<DetectionListAnnotationController>().Draw(screenTransform, palmDetections, isFlipped);
+    palmRectsAnnotation.GetComponent<RectListAnnotationController>().Draw(screenTransform, palmRects, isFlipped);
   }
 }
