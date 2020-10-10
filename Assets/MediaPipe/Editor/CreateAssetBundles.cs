6ebac7d0eac155669f6e53e8ed595e467f354eef
homuler <eulerdora.1810.mann@gmail.com>
2020-10-11 00:57:37 +0900

    feat: introduce AssetBundle

---
+++ Assets/MediaPipe/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = Path.Combine(Application.streamingAssetsPath, "AssetBundles");
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+                                        BuildAssetBundleOptions.None,
+                                        BuildTarget.StandaloneWindows);
+    }
+}
