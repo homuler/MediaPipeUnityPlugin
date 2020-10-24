718ef61b25607b3e74cb163604de381d0d30cbb7
homuler <eulerdora.1810.mann@gmail.com>
2020-10-24 14:18:03 +0900

    wip: add GpuBuffer constructor

--- Assets/MediaPipe/SDK/Scripts/Gpu/GpuBuffer.cs
+++ Assets/MediaPipe/SDK/Scripts/Gpu/GpuBuffer.cs
@@ -8,6 +8,9 @@ namespace Mediapipe {
  
     public GpuBuffer(GpuBufferPtr ptr, bool isOwner = true) : base(ptr, isOwner) {}
 
+    // TODO: investigate glTextureBuffer's state after initialization
+    public GpuBuffer(GlTextureBuffer glTextureBuffer) : base(UnsafeNativeMethods.MpGpuBufferCreate(glTextureBuffer.GetPtr())) {}
+
     protected override void Dispose(bool disposing) {
       if (_disposed) return;
 
