49cb3ab4029f74cc0c008a4127baf353d6f3e64b
homuler <eulerdora.1810.mann@gmail.com>
2020-11-04 00:42:12 +0900

    create Packet directory

--- Assets/MediaPipe/SDK/Scripts/PInvoke/NativeMethods/Framework/Packet_Unsafe.cs
+++ Assets/MediaPipe/SDK/Scripts/PInvoke/NativeMethods/Framework/Packet_Unsafe.cs
@@ -70,5 +70,22 @@ namespace Mediapipe {
     [DllImport (MediaPipeLibrary, ExactSpelling = true)]
     public static extern MpReturnCode mp_Packet__ValidateAsString(IntPtr packet, out IntPtr status);
     #endregion
+
+    #region SidePacket
+    [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+    public static extern MpReturnCode mp_SidePacket__(out IntPtr sidePacket);
+
+    [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+    public static extern void mp_SidePacket__delete(IntPtr sidePacket);
+
+    [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+    public static extern MpReturnCode mp_SidePacket__emplace__PKc_Rpacket(IntPtr sidePacket, string key, IntPtr packet);
+
+    [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+    public static extern MpReturnCode mp_SidePacket__at__PKc(IntPtr sidePacket, string key, out IntPtr packet);
+
+    [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+    public static extern MpReturnCode mp_SidePacket__erase__PKc(IntPtr sidePacket, string key, out int count);
+    #endregion
   }
 }
