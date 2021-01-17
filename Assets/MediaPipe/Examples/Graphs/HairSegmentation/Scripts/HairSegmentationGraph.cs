2140a3c826c9c65a054c471212ffd3d74ada7219
homuler <eulerdora.1810.mann@gmail.com>
2021-01-17 11:01:37 +0900

    perf(example): Calculator converts hair_mask into ImageFrame

--- Assets/MediaPipe/Examples/Graphs/HairSegmentation/Scripts/HairSegmentationGraph.cs
+++ Assets/MediaPipe/Examples/Graphs/HairSegmentation/Scripts/HairSegmentationGraph.cs
@@ -2,13 +2,13 @@ using Mediapipe;
 using UnityEngine;
 
 public class HairSegmentationGraph : DemoGraph {
-  private const string hairMaskStream = "hair_mask";
-  private OutputStreamPoller<GpuBuffer> hairMaskStreamPoller;
-  private GpuBufferPacket hairMaskPacket;
+  private const string hairMaskStream = "hair_mask_cpu";
+  private OutputStreamPoller<ImageFrame> hairMaskStreamPoller;
+  private ImageFramePacket hairMaskPacket;
 
   public override Status StartRun() {
-    hairMaskStreamPoller = graph.AddOutputStreamPoller<GpuBuffer>(hairMaskStream).ConsumeValueOrDie();
-    hairMaskPacket = new GpuBufferPacket();
+    hairMaskStreamPoller = graph.AddOutputStreamPoller<ImageFrame>(hairMaskStream).ConsumeValueOrDie();
+    hairMaskPacket = new ImageFramePacket();
 
     return graph.StartRun();
   }
@@ -24,37 +24,7 @@ public class HairSegmentationGraph : DemoGraph {
   }
 
   private ImageFrame FetchNextHairMask() {
-    if (!hairMaskStreamPoller.Next(hairMaskPacket)) {
-      Debug.LogWarning($"Failed to fetch next packet from {hairMaskStream}");
-      return null;
-    }
-
-    ImageFrame outputFrame = null;
-
-    var status = gpuHelper.RunInGlContext(() => {
-      var gpuFrame = hairMaskPacket.Get();
-      var gpuFrameFormat = gpuFrame.Format();
-      var sourceTexture = gpuHelper.CreateSourceTexture(gpuFrame);
-
-      outputFrame = new ImageFrame(
-        gpuFrameFormat.ImageFormatFor(), gpuFrame.Width(), gpuFrame.Height(), ImageFrame.kGlDefaultAlignmentBoundary);
-
-      gpuHelper.BindFramebuffer(sourceTexture);
-      var info = gpuFrameFormat.GlTextureInfoFor(0);
-
-      Gl.ReadPixels(0, 0, sourceTexture.width, sourceTexture.height, info.glFormat, info.glType, outputFrame.MutablePixelData());
-      Gl.Flush();
-
-      sourceTexture.Release();
-
-      return Status.Ok(false);
-    });
-
-    if (!status.ok) {
-      Debug.LogError(status.ToString());
-    }
-
-    return outputFrame;
+    return FetchNext(hairMaskStreamPoller, hairMaskPacket, hairMaskStream);
   }
 
   private void RenderAnnotation(WebCamScreenController screenController, ImageFrame hairMask) {
