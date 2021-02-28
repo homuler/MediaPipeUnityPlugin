1627ab1cbb63ffe2ad9a66ef1cada4c043bbabfd
homuler <eulerdora.1810.mann@gmail.com>
2021-02-28 13:52:00 +0900

    chore: remove unused `using` statements

--- Assets/MediaPipe/Examples/Graphs/BoxTracking/Scripts/BoxTrackingGraph.cs
+++ Assets/MediaPipe/Examples/Graphs/BoxTracking/Scripts/BoxTrackingGraph.cs
@@ -1,9 +1,3 @@
-using Mediapipe;
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using Google.Protobuf;
-
 public class BoxTrackingGraph : OfficialDemoGraph {
   protected override void PrepareDependentAssets() {
     PrepareDependentAsset("ssdlite_object_detection.bytes");
