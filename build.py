67fcb0aca0b38cf61ac9073b8fb9a1f071adbe7a
homuler <eulerdora.1810.mann@gmail.com>
2021-07-04 18:44:06 +0900

    fix: typo (debug -> dbg)

--- build.py
+++ build.py
@@ -358,7 +358,7 @@ class Argument:
     build_command_parser.add_argument('--android', choices=['arm', 'arm64'])
     build_command_parser.add_argument('--ios', choices=['arm64'])
     build_command_parser.add_argument('--resources', action=argparse.BooleanOptionalAction, default=True)
-    build_command_parser.add_argument('--compilation_mode', '-c', choices=['fastbuild', 'opt', 'debug'], default='opt')
+    build_command_parser.add_argument('--compilation_mode', '-c', choices=['fastbuild', 'opt', 'dbg'], default='opt')
     build_command_parser.add_argument('--opencv', choices=['local', 'cmake'], default='local', help='Decide to which OpenCV to link for Desktop native libraries')
     build_command_parser.add_argument('--opencv_deps', action='append', choices=['ffmpeg'], default=[], help='OpenCV Dependencies (only used when `--opencv=cmake`)')
     build_command_parser.add_argument('--include_opencv_libs', action='store_true', help='Include OpenCV\'s native libraries for Desktop')
