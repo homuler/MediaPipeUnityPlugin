ac8d5b5b42420181e3f06e5127be56bba038d997
homuler <eulerdora.1810.mann@gmail.com>
2021-09-05 11:16:25 +0900

    remove old Box Tracking sample

--- Assets/Mediapipe/Samples/Graphs/BoxTracking/Scripts/BoxTrackingGraph.cs
+++
@@ -1,6 +0,0 @@
-public class BoxTrackingGraph : OfficialDemoGraph {
-  protected override void PrepareDependentAssets() {
-    PrepareDependentAsset("ssdlite_object_detection.bytes");
-    PrepareDependentAsset("ssdlite_object_detection_labelmap.txt");
-  }
-}
