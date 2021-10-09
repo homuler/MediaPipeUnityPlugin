cfd18c0f9ff9933a91247d69e0b6933bc2d7282b
homuler <eulerdora.1810.mann@gmail.com>
2021-10-09 16:10:50 +0900

    refactor: fix lint warnings (FontAwesome/)

--- Assets/FontAwesome/UnicodeInlineText.cs
+++ Assets/FontAwesome/UnicodeInlineText.cs
@@ -1,44 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 using UnityEngine.UI;
 using System.Text.RegularExpressions;
 
 public class UnicodeInlineText : Text
 {
-  private bool disableDirty = false;
-  private Regex regexp = new Regex(@"\\u(?<Value>[a-zA-Z0-9]+)");
+  private bool _disableDirty = false;
+  private readonly Regex _regexp = new Regex(@"\\u(?<Value>[a-zA-Z0-9]+)");
 
   protected override void OnPopulateMesh(VertexHelper vh)
   {
-    string cache = text;
-    disableDirty = true;
+    var cache = text;
+    _disableDirty = true;
     text = Decode(text);
     base.OnPopulateMesh(vh);
     text = cache;
-    disableDirty = false;
+    _disableDirty = false;
   }
 
   private string Decode(string value)
   {
-    return regexp.Replace(value, m => ((char)int.Parse(m.Groups["Value"].Value, System.Globalization.NumberStyles.HexNumber)).ToString());
+    return _regexp.Replace(value, m => ((char)int.Parse(m.Groups["Value"].Value, System.Globalization.NumberStyles.HexNumber)).ToString());
   }
 
   public override void SetLayoutDirty()
   {
-    if (disableDirty) return;
+    if (_disableDirty)
+    {
+      return;
+    }
     base.SetLayoutDirty();
   }
 
   public override void SetVerticesDirty()
   {
-    if (disableDirty) return;
+    if (_disableDirty)
+    {
+      return;
+    }
     base.SetVerticesDirty();
   }
 
   public override void SetMaterialDirty()
   {
-    if (disableDirty) return;
+    if (_disableDirty)
+    {
+      return;
+    }
     base.SetMaterialDirty();
   }
 }
