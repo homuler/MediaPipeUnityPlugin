74cf1235c3a5be776e33e8ac215e10a33ce6714a
Junrou Nishida <homuler@users.noreply.github.com>
2022-03-03 15:04:18 +0900

    feat: implement StatusOrString

---
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/PInvoke/NativeMethods/External/Stdlib_Safe.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace Mediapipe
+{
+  internal static partial class SafeNativeMethods
+  {
+    [Pure, DllImport(MediaPipeLibrary, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_StatusOrString__ok(IntPtr statusOrString);
+  }
+}
--- mediapipe_api/external/stdlib.h
+++ mediapipe_api/external/stdlib.h
@@ -10,6 +10,7 @@
 #include <string>
 
 #include "mediapipe_api/common.h"
+#include "mediapipe_api/external/absl/statusor.h"
 
 extern "C" {
 
@@ -19,6 +20,13 @@ MP_CAPI(void) delete_array__PKc(const char* str);
 MP_CAPI(void) std_string__delete(std::string* str);
 MP_CAPI(MpReturnCode) std_string__PKc_i(const char* src, int size, std::string** str_out);
 MP_CAPI(void) std_string__swap__Rstr(std::string* src, std::string* dst);
+
+// StatusOr API
+MP_CAPI(void) mp_StatusOrString__delete(absl::StatusOr<std::string>* status_or_string);
+MP_CAPI(bool) mp_StatusOrString__ok(absl::StatusOr<std::string>* status_or_string);
+MP_CAPI(MpReturnCode) mp_StatusOrString__status(absl::StatusOr<std::string>* status_or_string, absl::Status** status_out);
+MP_CAPI(MpReturnCode) mp_StatusOrString__value(absl::StatusOr<std::string>* status_or_string, const char** value_out);
+MP_CAPI(MpReturnCode) mp_StatusOrString__bytearray(absl::StatusOr<std::string>* status_or_string, const char** value_out, int* size_out);
 
 }  // extern "C"
 
