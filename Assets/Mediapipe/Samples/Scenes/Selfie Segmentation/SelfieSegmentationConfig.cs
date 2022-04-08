7d6090de1ae39e040e9a463228ae73cc5a388b04
Junrou Nishida <homuler@users.noreply.github.com>
2022-04-08 16:27:28 +0900

    feat(sample): Selfie Segmentation (#522)
    * feat(sample): Selfie Segmentation
    
    * doc: update matrix
    
    * fix sample code

---
+++ Assets/Mediapipe/Samples/Scenes/Selfie Segmentation/SelfieSegmentationConfig.cs
@@ -0,0 +1,84 @@
+// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mediapipe.Unity.UI;
+
+namespace Mediapipe.Unity.SelfieSegmentation.UI
+{
+  public class SelfieSegmentationConfig : ModalContents
+  {
+    private const string _RunningModePath = "Scroll View/Viewport/Contents/Running Mode/Dropdown";
+    private const string _TimeoutMillisecPath = "Scroll View/Viewport/Contents/Timeout Millisec/InputField";
+
+    private SelfieSegmentationSolution _solution;
+    private Dropdown _runningModeInput;
+    private InputField _timeoutMillisecInput;
+
+    private bool _isChanged;
+
+    private void Start()
+    {
+      _solution = GameObject.Find("Solution").GetComponent<SelfieSegmentationSolution>();
+      InitializeContents();
+    }
+
+    public override void Exit()
+    {
+      GetModal().CloseAndResume(_isChanged);
+    }
+
+    public void SwitchRunningMode()
+    {
+      _solution.runningMode = (RunningMode)_runningModeInput.value;
+      _isChanged = true;
+    }
+
+    public void SetTimeoutMillisec()
+    {
+      if (int.TryParse(_timeoutMillisecInput.text, out var value))
+      {
+        _solution.timeoutMillisec = value;
+        _isChanged = true;
+      }
+    }
+
+    private void InitializeContents()
+    {
+      InitializeRunningMode();
+      InitializeTimeoutMillisec();
+    }
+
+    private void InitializeRunningMode()
+    {
+      _runningModeInput = gameObject.transform.Find(_RunningModePath).gameObject.GetComponent<Dropdown>();
+      _runningModeInput.ClearOptions();
+
+      var options = new List<string>(Enum.GetNames(typeof(RunningMode)));
+      _runningModeInput.AddOptions(options);
+
+      var currentRunningMode = _solution.runningMode;
+      var defaultValue = options.FindIndex(option => option == currentRunningMode.ToString());
+
+      if (defaultValue >= 0)
+      {
+        _runningModeInput.value = defaultValue;
+      }
+
+      _runningModeInput.onValueChanged.AddListener(delegate { SwitchRunningMode(); });
+    }
+
+    private void InitializeTimeoutMillisec()
+    {
+      _timeoutMillisecInput = gameObject.transform.Find(_TimeoutMillisecPath).gameObject.GetComponent<InputField>();
+      _timeoutMillisecInput.text = _solution.timeoutMillisec.ToString();
+      _timeoutMillisecInput.onValueChanged.AddListener(delegate { SetTimeoutMillisec(); });
+    }
+  }
+}
