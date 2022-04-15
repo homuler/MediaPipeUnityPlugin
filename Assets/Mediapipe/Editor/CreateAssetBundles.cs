ca7f0c50e25d95565a64f7e79584f117dc17d35e
Junrou Nishida <homuler@users.noreply.github.com>
2022-04-15 13:33:19 +0900

    lint

--- Assets/Mediapipe/Editor/CreateAssetBundles.cs
+++ Assets/Mediapipe/Editor/CreateAssetBundles.cs
@@ -1,19 +1,29 @@
-﻿using System.IO;
+﻿// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
+
 public class CreateAssetBundles
 {
-    [MenuItem("Assets/Build AssetBundles")]
-    internal static void BuildAllAssetBundles()
+  [MenuItem("Assets/Build AssetBundles")]
+  internal static void BuildAllAssetBundles()
+  {
+    var assetBundleDirectory = Application.streamingAssetsPath;
+    if (!Directory.Exists(assetBundleDirectory))
     {
-        string assetBundleDirectory = Application.streamingAssetsPath;
-        if(!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+      var _ = Directory.CreateDirectory(assetBundleDirectory);
+    }
+    _ = BuildPipeline.BuildAssetBundles(assetBundleDirectory,
                                         BuildAssetBundleOptions.None,
                                         EditorUserBuildSettings.activeBuildTarget);
-    }
+  }
 }
+
+#endif
