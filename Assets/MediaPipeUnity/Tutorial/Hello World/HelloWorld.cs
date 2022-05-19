c9e0317ab336b2f9515bc81935eef5c76d032fd9
Junrou Nishida <homuler@users.noreply.github.com>
2022-05-19 15:04:07 +0900

    feat: add scenes for tutorial (#582)
    * feat(tutorial): add the Hello World template
    
    * feat(tutorial): add Face Mesh template
    
    * export Tutorial assets

---
+++ Assets/MediaPipeUnity/Tutorial/Hello World/HelloWorld.cs
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// ATTENTION!: This code is for a tutorial and it's broken as is.
+
+using UnityEngine;
+
+namespace Mediapipe.Unity.Tutorial
+{
+  public class HelloWorld : MonoBehaviour { }
+}
