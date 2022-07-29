44202abf6710e809a1d36b262f85ccd3615fbf57
Martin Garbade <garbade.work@gmail.com>
2022-07-29 18:47:42 +0200

    add license

--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/PInvoke/NativeMethods/Framework/Format/Matrix_Unsafe.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/PInvoke/NativeMethods/Framework/Format/Matrix_Unsafe.cs
@@ -1,4 +1,8 @@
-
+// Copyright (c) 2021 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 using System;
 using System.Runtime.InteropServices;
@@ -7,14 +11,12 @@ namespace Mediapipe
 {
   internal static partial class UnsafeNativeMethods
   {
-   
     #region Packet
     [DllImport(MediaPipeLibrary, ExactSpelling = true)]
     public static extern MpReturnCode mp__MakeMatrixPacket__PKc_i(byte[] serializedMatrixData, int size, out IntPtr packet_out);
 
     [DllImport(MediaPipeLibrary, ExactSpelling = true)]
     public static extern MpReturnCode mp__MakeMatrixPacket_At__PA_i_Rt(byte[] serializedMatrixData, int size, IntPtr timestamp, out IntPtr packet_out);
-
     #endregion
   }
 }
