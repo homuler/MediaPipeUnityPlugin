3669c86ddcf9989bb42e497bd1d37c2531e92bec
Martin Garbade <garbade.work@gmail.com>
2022-07-29 19:07:15 +0200

    float_vector.cc -> faster vector allocation

--- mediapipe_api/framework/formats/float_vector.cc
+++ mediapipe_api/framework/formats/float_vector.cc
@@ -4,10 +4,7 @@
 
 MpReturnCode mp__MakeFloatVectorPacket__PA_i(const float* value, int size, mediapipe::Packet** packet_out) {
   TRY
-    std::vector<float> vector{};
-    for (auto i = 0; i < size; ++i) {
-      vector.push_back(value[i]);
-    }
+    std::vector<float> vector(value, value + size);
     *packet_out = new mediapipe::Packet{mediapipe::MakePacket<std::vector<float>>(vector)};
     RETURN_CODE(MpReturnCode::Success);
   CATCH_EXCEPTION
@@ -16,10 +13,7 @@ MpReturnCode mp__MakeFloatVectorPacket__PA_i(const float* value, int size, media
 MpReturnCode mp__MakeFloatVectorPacket_At__PA_i_Rt(const float* value, int size, mediapipe::Timestamp* timestamp,
                                                       mediapipe::Packet** packet_out) {
   TRY
-    std::vector<float> vector{};
-    for (auto i = 0; i < size; ++i) {
-      vector.push_back(value[i]);
-    }
+    std::vector<float> vector(value, value + size);
     *packet_out = new mediapipe::Packet{mediapipe::MakePacket<std::vector<float>>(vector).At(*timestamp)};
     RETURN_CODE(MpReturnCode::Success);
   CATCH_EXCEPTION
