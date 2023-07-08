fb3e6294ee7f495270526b618eb9f5a42186aa8b
Junrou Nishida <homuler@users.noreply.github.com>
2023-07-08 13:44:20 +0900

    feat: implement BaseOptions

---
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Tasks/Core/BaseOptions.cs
@@ -0,0 +1,78 @@
+// Copyright (c) 2023 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+namespace Mediapipe.Tasks.Core
+{
+  public sealed class BaseOptions
+  {
+    public enum Delegate
+    {
+      CPU,
+      GPU,
+    }
+
+    public Delegate delegateCase { get; } = Delegate.CPU;
+    public string modelAssetPath { get; } = string.Empty;
+    public byte[] modelAssetBuffer { get; } = null;
+
+    public BaseOptions(Delegate delegateCase = Delegate.CPU, string modelAssetPath = "", byte[] modelAssetBuffer = null)
+    {
+      this.delegateCase = delegateCase;
+      this.modelAssetPath = modelAssetPath;
+      this.modelAssetBuffer = modelAssetBuffer;
+    }
+
+    private Proto.Acceleration acceleration
+    {
+      get
+      {
+        switch (delegateCase)
+        {
+          case Delegate.CPU:
+            return new Proto.Acceleration
+            {
+              Tflite = new InferenceCalculatorOptions.Types.Delegate.Types.TfLite { },
+            };
+          case Delegate.GPU:
+            return new Proto.Acceleration
+            {
+              Gpu = new InferenceCalculatorOptions.Types.Delegate.Types.Gpu { },
+            };
+          default:
+            return null;
+        }
+      }
+    }
+
+    private Proto.ExternalFile modelAsset
+    {
+      get
+      {
+        var file = new Proto.ExternalFile { };
+
+        if (modelAssetPath != null)
+        {
+          file.FileName = modelAssetPath;
+        }
+        if (modelAssetBuffer != null)
+        {
+          file.FileContent = Google.Protobuf.ByteString.CopyFrom(modelAssetBuffer);
+        }
+
+        return file;
+      }
+    }
+
+    internal Proto.BaseOptions ToProto()
+    {
+      return new Proto.BaseOptions
+      {
+        ModelAsset = modelAsset,
+        Acceleration = acceleration,
+      };
+    }
+  }
+}
