23f08528696dd6ebcf13ea8e33c54d832f0639af
Junrou Nishida <homuler@users.noreply.github.com>
2023-07-08 13:43:08 +0900

    build: test code can access internal properties

---
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Mediapipe.Tests.EditMode")]
