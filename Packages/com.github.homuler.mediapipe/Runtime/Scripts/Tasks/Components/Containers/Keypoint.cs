aa8debae9650714c360f07c3af927acdb7bc8718
Junrou Nishida <homuler@users.noreply.github.com>
2023-07-22 16:05:38 +0900

    feat: implement Components.Containers.*

---
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Tasks/Components/Containers/Keypoint.cs
@@ -0,0 +1,45 @@
+// Copyright (c) 2023 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+namespace Mediapipe.Tasks.Components.Containers
+{
+  /// <summary>
+  ///   A keypoint, defined by the coordinates (x, y), normalized by the image dimensions.
+  /// </summary>
+  public readonly struct NormalizedKeypoint
+  {
+    /// <summary>
+    ///   x in normalized image coordinates.
+    /// </summary>
+    public readonly float x;
+    /// <summary>
+    ///   y in normalized image coordinates.
+    /// </summary>
+    public readonly float y;
+    /// <summary>
+    ///   optional label of the keypoint.
+    /// </summary>
+    public readonly string label;
+    /// <summary>
+    ///   optional score of the keypoint.
+    /// </summary>
+    public readonly float? score;
+
+    internal NormalizedKeypoint(float x, float y, string label, float? score)
+    {
+      this.x = x;
+      this.y = y;
+      this.label = label;
+      this.score = score;
+    }
+
+    public override string ToString()
+    {
+      var scoreStr = score == null ? "null" : $"{score}";
+      return $"{{ \"x\": {x}, \"y\": {y}, \"label\": \"{label}\", \"score\": {scoreStr} }}";
+    }
+  }
+}
