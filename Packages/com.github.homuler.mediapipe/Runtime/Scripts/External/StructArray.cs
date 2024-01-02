cc40ea6b383ab30915e30028a673f6c5ce82d99b
Junrou Nishida <homuler@users.noreply.github.com>
2024-01-03 00:38:01 +0900

    refactor: make StructArray generic

--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/External/StructArray.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/External/StructArray.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace Mediapipe
 {
   [StructLayout(LayoutKind.Sequential)]
-  internal readonly struct StructArray
+  internal readonly struct StructArray<T> where T : unmanaged
   {
     private readonly IntPtr _data;
     private readonly int _size;
@@ -15,7 +15,7 @@ namespace Mediapipe
       UnsafeNativeMethods.delete_array__Pf(_data);
     }
 
-    public List<T> Copy<T>() where T : unmanaged
+    public List<T> Copy()
     {
       var data = new List<T>(_size);
 
@@ -23,7 +23,7 @@ namespace Mediapipe
       return data;
     }
 
-    public void CopyTo<T>(List<T> data) where T : unmanaged
+    public void CopyTo(List<T> data)
     {
       data.Clear();
 
