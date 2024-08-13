676621987563cd68cbc8247de01ed61af1d1c071
Junrou Nishida <homuler@users.noreply.github.com>
2024-08-13 12:55:28 +0900

    fix: dispose callback arguments

--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/Tasks/Vision/ImageSegmenter/ImageSegmenter.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Tasks/Vision/ImageSegmenter/ImageSegmenter.cs
@@ -250,13 +250,13 @@ namespace Mediapipe.Tasks.Vision.ImageSegmenter
 
       return (PacketMap outputPackets) =>
       {
-        var outImagePacket = outputPackets.At<Image>(_IMAGE_OUT_STREAM_NAME);
+        using var outImagePacket = outputPackets.At<Image>(_IMAGE_OUT_STREAM_NAME);
         if (outImagePacket == null || outImagePacket.IsEmpty())
         {
           return;
         }
 
-        var image = outImagePacket.Get();
+        using var image = outImagePacket.Get();
         var timestamp = outImagePacket.TimestampMicroseconds() / _MICRO_SECONDS_PER_MILLISECOND;
 
         if (TryBuildImageSegmenterResult(outputPackets, ref segmentationResult))
