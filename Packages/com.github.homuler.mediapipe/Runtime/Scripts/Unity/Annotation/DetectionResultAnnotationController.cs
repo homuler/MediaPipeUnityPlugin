c8f61ce6b8fe4a2a6bddc203a40d842c644eb566
Junrou Nishida <homuler@users.noreply.github.com>
2025-01-12 18:30:07 +0900

    fix: acquire a lock when drawing DetectionResult annotations (#1306)

--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/DetectionResultAnnotationController.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/DetectionResultAnnotationController.cs
@@ -14,6 +14,7 @@ namespace Mediapipe.Unity
   {
     [SerializeField, Range(0, 1)] private float _threshold = 0.0f;
 
+    private readonly object _currentTargetLock = new object();
     private mptcc.DetectionResult _currentTarget;
 
     public void DrawNow(mptcc.DetectionResult target)
@@ -22,12 +23,24 @@ namespace Mediapipe.Unity
       SyncNow();
     }
 
-    public void DrawLater(mptcc.DetectionResult target) => UpdateCurrentTarget(target, ref _currentTarget);
+    public void DrawLater(mptcc.DetectionResult target) => UpdateCurrentTarget(target);
+
+    protected void UpdateCurrentTarget(mptcc.DetectionResult newTarget)
+    {
+      lock (_currentTargetLock)
+      {
+        newTarget.CloneTo(ref _currentTarget);
+        isStale = true;
+      }
+    }
 
     protected override void SyncNow()
     {
-      isStale = false;
-      annotation.Draw(_currentTarget, imageSize, _threshold);
+      lock (_currentTargetLock)
+      {
+        isStale = false;
+        annotation.Draw(_currentTarget, imageSize, _threshold);
+      }
     }
   }
 }
