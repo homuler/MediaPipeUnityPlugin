d96df61048d6f2f970aedce92a8f43d011925fd1
ishigurobot <81726718+ishigurobot@users.noreply.github.com>
2025-02-18 21:56:48 +0900

    Update Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Experimental/TextureFrame.cs
    Co-authored-by: Junrou Nishida <homuler@users.noreply.github.com>

--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Experimental/TextureFrame.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Experimental/TextureFrame.cs
@@ -93,7 +93,7 @@ namespace Mediapipe.Unity.Experimental
       }
       _glSyncToken?.Dispose();
       _ = _InstanceTable.Remove(_instanceId);
-      MonoBehaviour.Destroy(_texture);
+      UnityEngine.Object.Destroy(_texture);
     }
 
     public void CopyTexture(Texture dst) => Graphics.CopyTexture(_texture, dst);
